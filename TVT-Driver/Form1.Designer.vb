9949cfea924e74cdca1fbb42b5f5f5bea618ed3c
skeimeier <stefan-keimeier@t-online.de>
2013-09-11 20:36:31 +0200

    test vom 11.09.13

---
+++ TVT-Driver/Form1.Designer.vb
@@ -0,0 +1,218 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.SerialPort = New System.IO.Ports.SerialPort(Me.components)
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripLabel1 = New System.Windows.Forms.ToolStripLabel()
+        Me.cmbComPorts = New System.Windows.Forms.ToolStripComboBox()
+        Me.ToolStripButton1 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripLabel2 = New System.Windows.Forms.ToolStripLabel()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SerialPort
+        '
+        Me.SerialPort.PortName = "COM"
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripLabel1, Me.cmbComPorts, Me.ToolStripButton1, Me.ToolStripLabel2})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 237)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(788, 25)
+        Me.ToolStrip1.TabIndex = 1
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'ToolStripLabel1
+        '
+        Me.ToolStripLabel1.Name = "ToolStripLabel1"
+        Me.ToolStripLabel1.Size = New System.Drawing.Size(63, 22)
+        Me.ToolStripLabel1.Text = "Select Port"
+        '
+        'cmbComPorts
+        '
+        Me.cmbComPorts.Name = "cmbComPorts"
+        Me.cmbComPorts.Size = New System.Drawing.Size(75, 25)
+        '
+        'ToolStripButton1
+        '
+        Me.ToolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripButton1.Image = CType(resources.GetObject("ToolStripButton1.Image"), System.Drawing.Image)
+        Me.ToolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton1.Name = "ToolStripButton1"
+        Me.ToolStripButton1.Size = New System.Drawing.Size(71, 22)
+        Me.ToolStripButton1.Text = "Open Serial"
+        '
+        'ToolStripLabel2
+        '
+        Me.ToolStripLabel2.Name = "ToolStripLabel2"
+        Me.ToolStripLabel2.Size = New System.Drawing.Size(15, 22)
+        Me.ToolStripLabel2.Text = "C"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(102, 50)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(174, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Location = New System.Drawing.Point(12, 50)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Send ESC"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(102, 14)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(174, 20)
+        Me.TextBox2.TabIndex = 4
+        '
+        'Button3
+        '
+        Me.Button3.Enabled = False
+        Me.Button3.Location = New System.Drawing.Point(12, 90)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Set  Time"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 300
+        Me.Timer2.Tag = "0"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(12, 119)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(29, 23)
+        Me.Button4.TabIndex = 6
+        Me.Button4.Text = "On"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(47, 119)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(40, 23)
+        Me.Button5.TabIndex = 7
+        Me.Button5.Text = "Off"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"ESC[?25l - Command Display in Status Line OFF", "ESC[?25h - Command Display in Status Line ON (DEFAULT)", "ESC[?15h - Disable Status Line", "ESC[?15l - Enable Status Line (DEFAULT)", "ESC[?75h - Disable SCART Switch", "ESC[?75l - Enable SCART Switch", "ESC[?16h - Disable Display", "ESC[?16l - Enable Display (DEFAULT)", "ESC[>5h - Cursor Invisible", "ESC[>5l - Cursor not Invisible (DEFAULT)", "ESC[>11h - Cursor static", "ESC[>11l - Cursor blinking (DEFAULT)"})
+        Me.ComboBox1.Location = New System.Drawing.Point(326, 14)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(450, 21)
+        Me.ComboBox1.TabIndex = 8
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(323, 55)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Label1"
+        '
+        'Button1
+        '
+        Me.Button1.Enabled = False
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Send Text"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(788, 262)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents SerialPort As System.IO.Ports.SerialPort
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ToolStrip1 As System.Windows.Forms.ToolStrip
+    Friend WithEvents ToolStripButton1 As System.Windows.Forms.ToolStripButton
+    Friend WithEvents cmbComPorts As System.Windows.Forms.ToolStripComboBox
+    Friend WithEvents ToolStripLabel1 As System.Windows.Forms.ToolStripLabel
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents ToolStripLabel2 As System.Windows.Forms.ToolStripLabel
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ TVT-Driver/Form1.vb
@@ -0,0 +1,107 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        SerialPort.Write(TextBox2.Text)
+
+    End Sub
+
+   
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim availablePorts As Array
+        availablePorts = System.IO.Ports.SerialPort.GetPortNames()
+
+        For i = 0 To UBound(availablePorts)
+            cmbComPorts.Items.Add(availablePorts(i))
+        Next
+        cmbComPorts.SelectionStart = 0
+
+
+    End Sub
+
+    Private Sub cmbComPorts_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbComPorts.Click
+
+    End Sub
+
+    Private Sub ToolStripButton1_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ToolStripButton1.Click
+        If ToolStripButton1.Text.Contains("Close") Then
+            ToolStripButton1.Text = "Open"
+            SerialPort.Close()
+        Else
+            SerialPort.PortName = cmbComPorts.SelectedText
+            SerialPort.Open()
+            If SerialPort.IsOpen Then
+                ToolStripButton1.Text = "Close"
+            End If
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        If SerialPort.IsOpen Then
+            ToolStripLabel2.Text = "O"
+            Button1.Enabled = True
+            Button2.Enabled = TextBox1.Text.Length > 0
+            Button3.Enabled = True
+
+        Else
+            ToolStripLabel2.Text = "X"
+            Button1.Enabled = False
+            Button2.Enabled = False
+            Button3.Enabled = False
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        If TextBox1.Text.Length > 0 Then
+            SerialPort.Write(Chr(27) + "[" + TextBox1.Lines(0))
+        End If
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Dim datum As System.DateTime
+        Dim strDatum, dd, mm, yy As String
+        Dim strZeit, hh, mi, sec As String
+        datum = System.DateTime.Now
+        dd = datum.Day.ToString.PadLeft(2, "0")
+        mm = datum.Month.ToString.PadLeft(2, "0")
+        yy = (datum.Year - 2000).ToString
+        strDatum = dd + ";" + mm + ";" + yy
+        SerialPort.Write(Chr(27) + "[" + strDatum + "?t")
+
+        System.Threading.Thread.Sleep(300)
+
+        hh = datum.Hour.ToString.PadLeft(2, "0")
+        mi = datum.Minute.ToString.PadLeft(2, "0")
+        sec = datum.Second.ToString.PadLeft(2, "0")
+        strZeit = hh + ";" + mi + ";" + sec
+        SerialPort.Write(Chr(27) + "[" + strZeit + "?s")
+
+
+    End Sub
+
+    Private Sub Timer2_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer2.Tick
+        Timer2.Tag = 0
+        Timer2.Enabled = False
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        SerialPort.Write(Chr(27) + "[?75l")
+
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        SerialPort.Write(Chr(27) + "[?75h")
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Dim strESC, strLabel As String
+
+        strESC = ComboBox1.SelectedItem
+        strLabel = strESC.Substring(strESC.IndexOf("-") + 1)
+        Label1.Text = strLabel
+        strESC = strESC.Substring(4, strESC.IndexOf("-") - 5)
+        TextBox1.Text = strESC
+    End Sub
+End Class
